2710dee25b17e00391a5b7117f89403a9a953584
ASTIK <astik.ytt@gmail.com>
2024-01-18 23:43:10 +0100

    Add GULP configuration

---
+++ gulp/tasks/html.js
@@ -0,0 +1,31 @@
+import fileInclude from "gulp-file-include";
+import versionNumber from "gulp-version-number";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
+
+export const html = () => {
+	return app.gulp.src(app.path.src.html)
+		.pipe(app.plugins.plumber())
+		.pipe(fileInclude())
+		.pipe(
+			app.plugins.if(app.isBuild,
+				webpHtmlNosvg()
+			)
+		)
+		.pipe(
+			app.plugins.if(app.isBuild,
+				versionNumber({
+					"value": "%DT%",
+					"append": {
+						"key": "_v",
+						"cover": 0,
+						"to": [
+							"css",
+							"js",
+						],
+					}
+				})
+			)
+		)
+		.pipe(app.gulp.dest(app.path.build.html))
+		.pipe(app.plugins.browsersync.stream());
+}
